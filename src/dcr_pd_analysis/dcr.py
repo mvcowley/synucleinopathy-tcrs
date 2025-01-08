9fb0b88f79eaf0937edd2f9e3cd129768fc3122c
MVCowley <51127523+MVCowley@users.noreply.github.com>
2025-01-08 12:47:48 +0000

    Refactor filter samples to dcr

--- src/dcr_pd_analysis/dcr.py
+++ src/dcr_pd_analysis/dcr.py
@@ -38,3 +38,17 @@ def load_reps(path: str, glob: str, expected: int) -> list[tuple[str, pl.DataFra
     return [
         (f.split(".")[0].split("/")[-1], pl.read_csv(f, separator="\t")) for f in files
     ]
+
+
+def filter_samples(
+    reps: list[tuple[str, pl.DataFrame]], key: int
+) -> list[tuple[str, pl.DataFrame]]:
+    NAME_I = 0
+    SAMPLE_CODE_I = 2
+    INDIVIDUAL_I = -1
+    filtered_reps = [
+        rep
+        for rep in reps
+        if int(rep[NAME_I].split("_")[SAMPLE_CODE_I][INDIVIDUAL_I]) == key
+    ]
+    return filtered_reps
