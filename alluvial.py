c2d7780584afd061e2c7361e31045d17631e904c
MVCowley <51127523+MVCowley@users.noreply.github.com>
2025-01-13 16:19:57 +0000

    Remove old course grain function

---
+++ alluvial.py
@@ -0,0 +1,22 @@
+
+from dcr_pd_analysis import dcr, plot, stats
+
+if __name__ == "__main__":
+    alpha_reps = dcr.load_reps(
+        "../data/tcrseqgroup/translated/", glob="*PKD*alpha*tsv", expected=32
+    )
+    beta_reps = dcr.load_reps(
+        "../data/tcrseqgroup/translated/", glob="*PKD*beta*tsv", expected=32
+    )
+
+    N = 8
+    id_venn = {}
+    for i in range(1, N + 1):
+        for data, chain in zip([alpha_reps, beta_reps], ["alpha", "beta"]):
+            filtered = dcr.filter_samples(data, i)
+            seqs = dcr.get_seqs(filtered)
+            # cg_seqs = dcr.course_grain(seqs, ["HB", "ST"], "BR")
+            # venn = stats.get_venn(cg_seqs)
+            # labels = list(cg_seqs.keys())
+            # fig = plot.venn3(venn, *labels)
+            # fig.write_image(f"out/{i}_{chain}_venn.png", scale=5)
--- src/dcr_pd_analysis/dcr.py
+++ src/dcr_pd_analysis/dcr.py
@@ -55,55 +55,6 @@ def filter_samples(
     return filtered_reps
 
 
-#
-# def course_grain(
-#     reps: list[tuple[str, pl.DataFrame]], tissues: list[str]
-# ) -> list[tuple[str, pl.DataFrame]]:
-#     NAME_I = 0
-#     DF_I = 1
-#     SAMPLE_CODE_I = 2
-#     INDIVIDUAL_I = -1
-#     selected_tissues = [
-#         rep
-#         for rep in reps
-#         if rep[NAME_I].split("_")[SAMPLE_CODE_I][:INDIVIDUAL_I] in tissues
-#     ]
-#
-#     if len(selected_tissues) <= 1:
-#         warnings.warn(
-#             f"Only {len(selected_tissues)} tissues matched your search. No course graining applied"
-#         )
-#         return reps
-#
-#     reps = [
-#         rep
-#         for rep in reps
-#         if rep[NAME_I].split("_")[SAMPLE_CODE_I][:INDIVIDUAL_I] not in tissues
-#     ]
-#
-#     base_name = selected_tissues[0][NAME_I]
-#     base_df = selected_tissues[0][DF_I].fill_null("missing")
-#     print("Before:", base_df.shape)
-#     columns = [
-#         i
-#         for i in base_df.columns
-#         if i not in ["sequence_id", "duplicate_count", "av_UMI_cluster_size"]
-#     ]
-#     for selected_tissue in selected_tissues[1:]:
-#         print(f"Joining {base_name} to {selected_tissue[NAME_I]}")
-#         print(base_df)
-#         joining_df = selected_tissue[DF_I].fill_null("missing")
-#         print(joining_df)
-#         assert base_df.columns == joining_df.columns
-#         assert base_df.dtypes == joining_df.dtypes
-#         base_df.join(joining_df, on=columns, how="full")
-#
-#     print("After:")
-#     print(selected_tissues[1][1].shape)
-#     print(base_df.shape)
-#
-
-
 def get_seqs(reps: list[tuple[str, pl.DataFrame]]) -> dict[str, list[str]]:
     NAME_I = 0
     DF_I = 1
