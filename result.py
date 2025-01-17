da9471a02698c3e0509e051639877c88b17a27cc
MVCowley <51127523+MVCowley@users.noreply.github.com>
2025-01-17 13:43:01 +0000

    Load results of pkd query

---
+++ result.py
@@ -0,0 +1,12 @@
+import pathlib
+
+import polars as pl
+
+if __name__ == "__main__":
+    path = pathlib.Path("out/query/result")
+    files = path.glob("*.csv")
+    data = {
+        file.resolve().__str__().split("/")[-1].split(".")[0]: pl.read_csv(file)
+        for file in files
+    }
+    print(data)
