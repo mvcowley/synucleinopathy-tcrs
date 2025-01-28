f6c407387007b17c654630f10b965ea1b9327681
MVCowley <51127523+MVCowley@users.noreply.github.com>
2025-01-28 01:00:20 +0000

    Swap to welch's t test

--- src/dcr_pd_analysis/plot/annotate.py
+++ src/dcr_pd_analysis/plot/annotate.py
@@ -86,7 +86,7 @@ def add_p_value_annotation(
         ]
 
         # Get the p-value
-        pvalue = stats.mannwhitneyu(filtered_data0, filtered_data1)[1]
+        pvalue = stats.ttest_ind(filtered_data0, filtered_data1, equal_var=False)[1]
         if pvalue >= 0.05:
             symbol = "ns"
         elif pvalue >= 0.01:
